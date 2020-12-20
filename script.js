dbca86b543efed076e855ddd94a8ace0f968114b
VladimirMahlin <46395628+VladimirMahlin@users.noreply.github.com>
2020-12-20 14:15:38 +0200

    Update script.js

--- script.js
+++ script.js
@@ -23,126 +23,18 @@
  * LinkedIn: https://www.linkedin.com/in/creativetech
  ********************************************************************/
 
-const video = document.getElementById('webcam');
-const liveView = document.getElementById('liveView');
 const demosSection = document.getElementById('demos');
 
-// An object to configure parameters to set for the bodypix model.
-// See github docs for explanations.
-const bodyPixProperties = {
-  architecture: 'MobileNetV1',
-  outputStride: 16,
-  multiplier: 0.75,
-  quantBytes: 4
-};
-
-// An object to configure parameters for detection. I have raised
-// the segmentation threshold to 90% confidence to reduce the
-// number of false positives.
-const segmentationProperties = {
-  flipHorizontal: false,
-  internalResolution: 'high',
-  segmentationThreshold: 0.9
-};
-
-
-// This array will hold the colours we wish to use to highlight different body parts we find.
-// RGBA (Red, Green, Blue, and Alpha (transparency) channels can be specified).
-const colourMap = [];
-
-// Left_face
-colourMap.push({r: 244, g: 67, b: 54, a: 255});
-// Right_face
-colourMap.push({r: 183, g: 28, b: 28, a: 255});
-// left_upper_arm_front
-colourMap.push({r: 233, g: 30, b: 99, a: 255});
-// left_upper_arm_back  
-colourMap.push({r: 136, g: 14, b: 79, a: 255});
-// right_upper_arm_front
-colourMap.push({r: 233, g: 30, b: 99, a: 255});
-// 	right_upper_arm_back
-colourMap.push({r: 136, g: 14, b: 79, a: 255});
-// 	left_lower_arm_front
-colourMap.push({r: 233, g: 30, b: 99, a: 255});
-// 	left_lower_arm_back
-colourMap.push({r: 136, g: 14, b: 79, a: 255});
-// right_lower_arm_front
-colourMap.push({r: 233, g: 30, b: 99, a: 255});
-// right_lower_arm_back
-colourMap.push({r: 136, g: 14, b: 79, a: 255});
-// left_hand 
-colourMap.push({r: 156, g: 39, b: 176, a: 255});
-// right_hand
-colourMap.push({r: 156, g: 39, b: 176, a: 255});
-// torso_front
-colourMap.push({r: 63, g: 81, b: 181, a: 255}); 
-// torso_back 
-colourMap.push({r: 26, g: 35, b: 126, a: 255});
-// left_upper_leg_front
-colourMap.push({r: 33, g: 150, b: 243, a: 255});
-// left_upper_leg_back
-colourMap.push({r: 13, g: 71, b: 161, a: 255});
-// right_upper_leg_front
-colourMap.push({r: 33, g: 150, b: 243, a: 255});
-// right_upper_leg_back
-colourMap.push({r: 13, g: 71, b: 161, a: 255});
-// left_lower_leg_front
-colourMap.push({r: 0, g: 188, b: 212, a: 255});
-// left_lower_leg_back
-colourMap.push({r: 0, g: 96, b: 100, a: 255});
-// right_lower_leg_front
-colourMap.push({r: 0, g: 188, b: 212, a: 255});
-// right_lower_leg_back
-colourMap.push({r: 0, g: 188, b: 212, a: 255});
-// left_feet
-colourMap.push({r: 255, g: 193, b: 7, a: 255});
-// right_feet
-colourMap.push({r: 255, g: 193, b: 7, a: 255});
-
-
-// A function to render returned segmentation data to a given canvas context.
-function processSegmentation(canvas, segmentation) {
-  var ctx = canvas.getContext('2d');
-  
-  var imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-  var data = imageData.data;
-  
-  let n = 0;
-  for (let i = 0; i < data.length; i += 4) {
-    if (segmentation.data[n] !== -1) {
-      data[i] = colourMap[segmentation.data[n]].r;     // red
-      data[i + 1] = colourMap[segmentation.data[n]].g; // green
-      data[i + 2] = colourMap[segmentation.data[n]].b; // blue
-      data[i + 3] = colourMap[segmentation.data[n]].a; // alpha
-    } else {
-      data[i] = 0;    
-      data[i + 1] = 0;
-      data[i + 2] = 0;
-      data[i + 3] = 0;
-    }
-    n++;
-  }
-  
-  ctx.putImageData(imageData, 0, 0);
-}
-
-
-
-// Let's load the model with our parameters defined above.
-// Before we can use bodypix class we must wait for it to finish
-// loading. Machine Learning models can be large and take a moment to
-// get everything needed to run.
-var modelHasLoaded = false;
 var model = undefined;
 
-model = bodyPix.load(bodyPixProperties).then(function (loadedModel) {
+// Before we can use COCO-SSD class we must wait for it to finish
+// loading. Machine Learning models can be large and take a moment to
+// get everything needed to run.
+cocoSsd.load().then(function (loadedModel) {
   model = loadedModel;
-  modelHasLoaded = true;
   // Show demo section now model is ready to use.
   demosSection.classList.remove('invisible');
 });
-
-
 
 
 /********************************************************************
@@ -163,26 +55,42 @@ for (let i = 0; i < imageContainers.length; i++) {
 
 // When an image is clicked, let's classify it and display results!
 function handleClick(event) {
-  if (!modelHasLoaded) {
+  if (!model) {
+    console.log('Wait for model to load before clicking!');
     return;
   }
   
-  // We can call model.segmentPerson as many times as we like with
+  // We can call model.classify as many times as we like with
   // different image data each time. This returns a promise
   // which we wait to complete and then call a function to
   // print out the results of the prediction.
-  model.segmentPersonParts(event.target, segmentationProperties).then(function(segmentation) {
-    console.log(segmentation);
-    
-    // Lets create a canvas to render our findings.
-    var canvas = document.createElement('canvas');
-    canvas.width = segmentation.width;
-    canvas.height = segmentation.height;
+  model.detect(event.target).then(function (predictions) {
+    // Lets write the predictions to a new paragraph element and
+    // add it to the DOM.
+    console.log(predictions);
+    for (let n = 0; n < predictions.length; n++) {
+      // Description text
+      const p = document.createElement('p');
+      p.innerText = predictions[n].class  + ' - with ' 
+          + Math.round(parseFloat(predictions[n].score) * 100) 
+          + '% confidence.';
+      // Positioned at the top left of the bounding box.
+      // Height is whatever the text takes up.
+      // Width subtracts text padding in CSS so fits perfectly.
+      p.style = 'left: ' + predictions[n].bbox[0] + 'px;' + 
+          'top: ' + predictions[n].bbox[1] + 'px; ' + 
+          'width: ' + (predictions[n].bbox[2] - 10) + 'px;';
 
-    processSegmentation(canvas, segmentation);
+      const highlighter = document.createElement('div');
+      highlighter.setAttribute('class', 'highlighter');
+      highlighter.style = 'left: ' + predictions[n].bbox[0] + 'px;' +
+          'top: ' + predictions[n].bbox[1] + 'px;' +
+          'width: ' + predictions[n].bbox[2] + 'px;' +
+          'height: ' + predictions[n].bbox[3] + 'px;';
 
-    // Add our canvas to the DOM.
-    event.target.parentNode.appendChild(canvas);
+      event.target.parentNode.appendChild(highlighter);
+      event.target.parentNode.appendChild(p);
+    }
   });
 }
 
@@ -194,7 +102,8 @@ function handleClick(event) {
 // https://tensorflow-js-image-classification.glitch.me/
 ********************************************************************/
 
-var previousSegmentationComplete = true;
+const video = document.getElementById('webcam');
+const liveView = document.getElementById('liveView');
 
 // Check if webcam access is supported.
 function hasGetUserMedia() {
@@ -202,29 +111,25 @@ function hasGetUserMedia() {
     navigator.mediaDevices.getUserMedia);
 }
 
+// Keep a reference of all the child elements we create
+// so we can remove them easilly on each render.
+var children = [];
 
-// This function will repeatidly call itself when the browser is ready to process
-// the next frame from webcam.
-function predictWebcam() {
-  if (previousSegmentationComplete) {
-    // Copy the video frame from webcam to a tempory canvas in memory only (not in the DOM).
-    videoRenderCanvasCtx.drawImage(video, 0, 0);
-    previousSegmentationComplete = false;
-    // Now classify the canvas image we have available.
-    model.segmentPersonParts(videoRenderCanvas, segmentationProperties).then(function(segmentation) {
-      processSegmentation(webcamCanvas, segmentation);
-      previousSegmentationComplete = true;
-    });
-  }
 
-  // Call this function again to keep predicting when the browser is ready.
-  window.requestAnimationFrame(predictWebcam);
+// If webcam supported, add event listener to button for when user
+// wants to activate it.
+if (hasGetUserMedia()) {
+  const enableWebcamButton = document.getElementById('webcamButton');
+  enableWebcamButton.addEventListener('click', enableCam);
+} else {
+  console.warn('getUserMedia() is not supported by your browser');
 }
 
 
 // Enable the live webcam view and start classification.
 function enableCam(event) {
-  if (!modelHasLoaded) {
+  if (!model) {
+    console.log('Wait! Model not loaded yet.')
     return;
   }
   
@@ -238,38 +143,53 @@ function enableCam(event) {
 
   // Activate the webcam stream.
   navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
-    video.addEventListener('loadedmetadata', function() {
-      // Update widths and heights once video is successfully played otherwise
-      // it will have width and height of zero initially causing classification
-      // to fail.
-      webcamCanvas.width = video.videoWidth;
-      webcamCanvas.height = video.videoHeight;
-      videoRenderCanvas.width = video.videoWidth;
-      videoRenderCanvas.height = video.videoHeight;
-    });
-    
     video.srcObject = stream;
-    
     video.addEventListener('loadeddata', predictWebcam);
   });
 }
 
 
-// Lets create a canvas to render our findings to the DOM.
-var webcamCanvas = document.createElement('canvas');
-webcamCanvas.setAttribute('class', 'overlay');
-liveView.appendChild(webcamCanvas);
+function predictWebcam() {
+  // Now let's start classifying the stream.
+  model.detect(video).then(function (predictions) {
+    // Remove any highlighting we did previous frame.
+    for (let i = 0; i < children.length; i++) {
+      liveView.removeChild(children[i]);
+    }
+    children.splice(0);
+    
+    // Now lets loop through predictions and draw them to the live view if
+    // they have a high confidence score.
+    for (let n = 0; n < predictions.length; n++) {
+      // If we are over 66% sure we are sure we classified it right, draw it!
+      if (predictions[n].score > 0.66) {
+        const p = document.createElement('p');
+        p.innerText = predictions[n].class  + ' - with ' 
+            + Math.round(parseFloat(predictions[n].score) * 100) 
+            + '% confidence.';
+        // Draw in top left of bounding box outline.
+        p.style = 'left: ' + predictions[n].bbox[0] + 'px;' +
+            'top: ' + predictions[n].bbox[1] + 'px;' + 
+            'width: ' + (predictions[n].bbox[2] - 10) + 'px;';
 
-// We will also create a tempory canvas to render to that is in memory only
-// to store frames from the web cam stream for classification.
-var videoRenderCanvas = document.createElement('canvas');
-var videoRenderCanvasCtx = videoRenderCanvas.getContext('2d');
+        // Draw the actual bounding box.
+        const highlighter = document.createElement('div');
+        highlighter.setAttribute('class', 'highlighter');
+        highlighter.style = 'left: ' + predictions[n].bbox[0] + 'px; top: '
+            + predictions[n].bbox[1] + 'px; width: ' 
+            + predictions[n].bbox[2] + 'px; height: '
+            + predictions[n].bbox[3] + 'px;';
 
-// If webcam supported, add event listener to button for when user
-// wants to activate it.
-if (hasGetUserMedia()) {
-  const enableWebcamButton = document.getElementById('webcamButton');
-  enableWebcamButton.addEventListener('click', enableCam);
-} else {
-  console.warn('getUserMedia() is not supported by your browser');
+        liveView.appendChild(highlighter);
+        liveView.appendChild(p);
+        
+        // Store drawn objects in memory so we can delete them next time around.
+        children.push(highlighter);
+        children.push(p);
+      }
+    }
+    
+    // Call this function again to keep predicting when the browser is ready.
+    window.requestAnimationFrame(predictWebcam);
+  });
 }
